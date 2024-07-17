09452f88e10a5fe98dc634ebb72bb2569cbcfe9d
nazifa24 <145958249+nazifa24@users.noreply.github.com>
2024-07-17 15:31:18 +1000

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,5 @@
+// Example of a simple script to handle form submission
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Form submitted!');
+});
